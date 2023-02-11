8844038d6ce217baa6dc288decd5312625f9d20f
Taha Mahmoud <dr.taha.libya@gmail.com>
2023-02-11 19:15:12 +0200

    private attributes parser and currentRoom added

---
+++ Game2.java
@@ -0,0 +1,18 @@
+/**
+ * this is a rewrite of zuul game adventure.
+ * my method with this book is to rewrite  the code myself.
+ * 
+ * @author taha mahmoud
+ * @version 11.2.2023
+ */
+public class Game2
+{
+    private Parser parser;
+    private Room currentRoom;
+
+
+
+
+
+
+}
