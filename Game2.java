bb6d2b92924dede59817f651376f23e283430668
Taha Mahmoud <dr.taha.libya@gmail.com>
2023-02-12 20:25:41 +0200

    exits are added no other classes touched

--- Game2.java
+++ Game2.java
@@ -46,16 +46,59 @@ public class Game2
         office.setExits(null, null, null, lab);
         pub.setExits(null, outside, null, null);
         
-        //set current room
+        //start the game out side
         currentRoom = outside;
     }
     
+    /**
+     * play
+     */
+    
+    public void play()
+    {
+        printWlcome();
+        
+        boolean finished = false;
+            while (!finished)
+            {
+                Command2 command = parser.getCommand();
+                finished = processCommand(command);
+            }
+            System.out.println("Thankyou for playing, Goodbye");
+            
+            
+    }
+   
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome()
+    {
+        System.out.println();
+        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Type 'help' if you need help.");
+        System.out.println();
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exits: ");
+        if(currentRoom.northExit != null){
+            System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null){
+            System.out.print("east ");
+        }
+        if(currentRoom.southExit != null){
+            System.out.print("south ");
+        }
+        if(currentRoom.westExit != null){
+            System.out.print("west ");
+        }
+        System.out.println();
     
     
-    
 
 
 
 
 
-}
+    }
