c2c298ba20a55e1ed6cedf9fa0e1a062de236220
Taha Mahmoud <dr.taha.libya@gmail.com>
2023-02-12 20:05:17 +0200

    merge

--- Game2.java
+++ Game2.java
@@ -7,11 +7,52 @@
  */
 public class Game2
 {
-    private Parser parser;
-    private Room currentRoom;
+    private Parser2 parser;
+    private Room2 currentRoom;
     
-    ;
-
+    /**
+     * kick the game and create the rooms
+     */
+    public Game2()
+    {
+        createRooms();
+        parser = new Parser2();
+    
+        {
+    
+        }
+    
+    }
+    
+    /**
+     * create rooms with exits
+     */
+    public void createRooms()
+    {
+        Room outside, theater, pub, lab, office;
+        // create rooms
+        
+        outside = new Room2("out side the main enterence of the university");
+        theater = new Room2("in a lecture theater");
+        pub = new Room2("in the campus pub");
+        lab = new Room2("in a computing lab");
+        office = new Room2("in the computing admin office");
+        
+        // set exits
+        
+        outside.setExits(null, theater, lab, pub);
+        theater.setExits(null, null, null, outside);
+        lab.setExits(outside, office, null, null);
+        office.setExits(null, null, null, lab);
+        pub.setExits(null, outside, null, null);
+        
+        //set current room
+        currentRoom = outside;
+    }
+    
+    
+    
+    
 
 
 
